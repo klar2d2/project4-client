f0dc8216b4e0ade38f2c741ffd7fc1405297ead7
madmonkeyaudio-js <jonathangross@Jonathans-MacBook-Pro.local>
2019-10-07 13:09:59 -0700

    refactor

---
+++ src/components/Chat.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Chat extends Component {
+    render() {
+        return (
+            <div>
+                <input type="text"/>
+                <button>Submit</button>
+            </div>
+        )
+    }
+}
+
+export default Chat
