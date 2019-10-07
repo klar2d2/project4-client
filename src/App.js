29c4c458410c962181b3fe13d3066dea269a19d5
madmonkeyaudio-js <jonathangross@Jonathans-MacBook-Pro.local>
2019-10-07 15:18:45 -0700

    add socket.io test color change

--- src/App.js
+++ src/App.js
@@ -1,36 +1,47 @@
 import React from 'react';
 import './App.css';
-import Chat from './components/Chat'
-import Sockjs from 'sockjs-client';
+import socketIOClient from 'socket.io-client'
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-
-    const sock = new Sockjs('https://localhost:3000');
-
-    sock.onopen = () => {
-        console.log('connection open');
-    };
-
-    sock.onmessage = e => {
-        console.log('message recieved:', e.data);
-    }
-
-    sock.onclose = () => {
-        console.log('close')
-    }
-
+  constructor() {
+    super();
     this.state = {
-      actions: sock,
-      messages: []
-  }
+      endpoint: 'localhost:3001', 
+      color: 'white'
+    }
+}
+
+send = () => {
+  const socket = socketIOClient(this.state.endpoint);
+  socket.emit('change color', this.state.color)
+}
+
+setColor = (color) => {
+  this.setState({ color })
+}
+
+componentDidMount = () => {
+  const socket = socketIOClient(this.state.endpoint);
+  setInterval(this.send(), 1000)
+  socket.on('change color', (col) => {
+    document.body.style.backgroundColor = col
+  })
 }
   render() {
+
+    const socket = socketIOClient(this.state.endpoint)
+    socket.on('change color', (col) => {
+      document.body.style.backgroundColor = col;
+    })
+
     return (
       <div className="App">
-        <Chat {... this.state }/>
+        <div style={{ textAlign: 'center'}}>
+          <button onClick={() => this.send() }>Change Color</button>
+
+          <button id="blue" onClick = {() => this.setColor('blue')}>Blue</button>
+          <button id="red" onClick={ () => this.setColor('red') }>Red</button>
+        </div>
       </div>
     );
   }
