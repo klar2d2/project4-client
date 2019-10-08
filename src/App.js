6a44c5569fe6c609887cc2aa8ecc84ca8fe2859e
madmonkeyaudio-js <jonathangross@Jonathans-MacBook-Pro.local>
2019-10-08 09:59:57 -0700

    creating message db call

--- src/App.js
+++ src/App.js
@@ -1,67 +1,16 @@
 import React from 'react';
 import './App.css';
-import socketIOClient from 'socket.io-client'
+import Chat from './components/Chat'
+
 
 class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      endpoint: 'localhost:3001', 
-      messages: ['Enter text here'], 
-      input: ''
-  }
-}
-
-formOnSubmit = (e) => {
-  e.preventDefault();
-  let messageArray = this.state.messages;
-  messageArray.push(this.state.input)
-  
-  const socket = socketIOClient(this.state.endpoint);
-  socket.emit('add message', this.state.input)
-}
-
-
-handleChange = (e) => {
-  this.setState({
-    [e.target.name]: e.target.value,
-  })
-}
-
-setMessages = () => {
-  
-}
-
-componentDidUpdate(prevProps, prevState) {
-  if (this.state.messages !== prevState.messages) {
-      this.setMessages();
-  }
-}
-
+ 
 
   render() {
-    const socket = socketIOClient(this.state.endpoint)
-    socket.on('add message', (mssg) => {
-      console.log(mssg)
-    })
-    
-    let messagesDiv = this.state.messages.map((text, idx) => {
-      return (
-        <div key={idx}>
-          <p>{text}</p>
-        </div>
-      )
-    })
 
     return (
       <div className="App">
-        <div style={{ textAlign: 'center'}}>
-          {messagesDiv}
-          <form onSubmit = { this.formOnSubmit }>
-            <input type="text" name="input" onChange={ this.handleChange }/>
-            <input type="submit"/>
-          </form>
-        </div>
+        <Chat />
       </div>
     );
   }
--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,12 +1,80 @@
 import React, { Component } from 'react';
+import socketIOClient from 'socket.io-client'
+import axios from 'axios'
 
 class Chat extends Component {
-   
+    constructor() {
+        super();
+        this.state = {
+          messages: ['Enter text here'], 
+          input: '',
+          socket: socketIOClient('localhost:3001'), 
+          goatId: null
+      }
+    }
+
+    componentDidMount(){
+            this.state.socket.on('add message', (mssg) => {
+            console.log(mssg)
+        })
+        this.callMessageDb()
+    }
+
+    callMessageDb = (userId, goatId) => {
+        axios.get('/messages', {
+            userId,
+            goatId
+        })
+        .then(response => {
+            this.setState({
+                messages: response.data
+            })
+        })
+        .catch(err => {
+            console.log(err)
+        })
+    }
+
+    formOnSubmit = (e) => {
+        e.preventDefault();
+        let messageArray = this.state.messages;
+        messageArray.push(this.state.input)
+        this.state.socket.emit('add message', this.state.input)
+    }
+
+    updateReciever = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value,
+        })
+    }
     render() {
+
+    let messagesDiv = this.state.messages.map((text, idx) => {
+      return (
+        <div key={idx}>
+          <p>{text}</p>
+        </div>
+      )
+    })
         return (
             <div className="chat-container">
-               
-               
+                <div style={{ textAlign: 'center'}}>
+                {messagesDiv}
+                <form>
+                    <input type="text" onChange = {this.updateReciver}/>
+                    <input type="submit"/>
+                </form>
+                <form onSubmit = { this.formOnSubmit }>
+                    <input type="text" name="input" onChange={ this.handleChange }/>
+                    <input type="submit"/>
+                </form>
+                </div>
             </div>
         )
     }
