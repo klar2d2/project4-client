563566c8ef052486bae6e6da858cf7bf941be77c
gtole342 <gtole342@gmail.com>
2019-10-08 21:14:16 -0700

    Added constants file

--- src/Components/Subcomponents/Appointments.jsx
+++ src/Components/Subcomponents/Appointments.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 
 class Appointments extends Component {
+  constructor(props){
+    super(props);
+  }
   render(){
     return(
       <div>Appointments</div>
