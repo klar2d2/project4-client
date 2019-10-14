b081e51b7b70f70c6bf1509210bcb1d153c21790
madmonkeyaudio-js <jonathangross@Jonathans-MBP.hsd1.wa.comcast.net>
2019-10-13 20:44:50 -0700

    copyright symbol

--- src/Components/Subcomponents/Footer.jsx
+++ src/Components/Subcomponents/Footer.jsx
@@ -4,7 +4,7 @@ class Footer extends Component {
     render() {
         return (
             <div className="footer">
-               <p className="footer-text"><a href="#">Copyright</a> of Three Billy Goats Gruff</p> 
+               <p className="footer-text"> &#169; of Three Billy Goats Gruff</p> 
             </div>
         )
     }
